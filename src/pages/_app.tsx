671ecfeabf674b6460cf865714e33c86474d4abf
Zulvan Avito <shawnanggara@gmail.com>
2025-07-13 00:55:20 +0800

    Add site metadata and update logo path
    Renamed logo image to 'logo.png' and updated favicon reference in _app.tsx. Added Head component to set page title and meta description for improved SEO.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "sonner";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Plastira - Jadilah Pahlawan Lingkungan!</title>
+        <meta
+          name="description"
+          content="Plastira adalah platform yang menghubungkan masyarakat dengan petugas kebersihan untuk pengelolaan sampah yang lebih baik."
+        />
+        <link rel="icon" href="/img/logo.png" />
+      </Head>
+
       <Component {...pageProps} />
       <Toaster richColors position="top-center" />
     </>
