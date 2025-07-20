eed201e99806d8b388450b4160d6322fc44b8cd5
Zulvan Avito <shawnanggara@gmail.com>
2025-07-20 15:51:42 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="dicoding:email" content="shawnanggara@gmail.com"></meta>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
